5100c8ca89ecd086988c80c718f1e7431a6524ed
alejandrachoque <alejandra75choque@gmail.com>
2023-08-25 20:13:30 -0300

    subir archivos de pys

---
+++ app.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const app= express();
+
+const path=require('path');
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/index.html'))
+})
+
+app.listen(3000,()=>console.log("Servidor funcionando"));
